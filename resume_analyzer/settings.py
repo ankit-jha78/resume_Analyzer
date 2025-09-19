f0a0bba6f549163d30bea652851ce2566b89d5e8
Ankit Kumar Jha <140503877+ankit-jha78@users.noreply.github.com>
2025-09-19 20:44:32 +0530

    Update settings.py

--- resume_analyzer/settings.py
+++ resume_analyzer/settings.py
@@ -92,7 +92,6 @@ DATABASES = {
 
 
 STATIC_URL='/static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
