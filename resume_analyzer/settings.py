c56dfb23d980bd265e17a9bd29a415bb43429aa5
Ankit Kumar Jha <140503877+ankit-jha78@users.noreply.github.com>
2025-09-19 20:48:20 +0530

    Update settings.py

--- resume_analyzer/settings.py
+++ resume_analyzer/settings.py
@@ -25,7 +25,7 @@ import os
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["35.160.120.126","resume-analyzer-tmas.onrender.com"]
+ALLOWED_HOSTS = ["35.160.120.126","resume-analyzer-1-hlwz.onrender.com"]
 
 from dotenv import load_dotenv
 
